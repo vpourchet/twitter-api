ffd98331ae7e755a830e1e831c8db4545d436f9e
Valentin Pourchet <valentin.pourchet@gmail.com>
2020-06-19 11:54:00 +0200

    New version

--- app/apis/tweets.py
+++ app/apis/tweets.py
@@ -1,14 +1,21 @@
 # app/apis/tweets.py
 from flask_restx import Namespace, Resource, fields
+from flask import abort
 from app.db import repository
+from app.models import Tweet
 
 api = Namespace('tweets')
 
-tweet = api.model('Tweet', {
+json_tweet = api.model('Tweet', {
     'id': fields.Integer,
     'text': fields.String,
     'created_at': fields.DateTime
 })
+
+json_new_tweet = api.model('New tweet', {
+    'text': fields.String(required=True)
+})
+
 
 @api.route('/<int:id>')
 @api.response(404, 'Tweet not found')
@@ -16,34 +23,37 @@ tweet = api.model('Tweet', {
 @api.param('text', 'Tweet text')
 @api.param('created_at', 'Tweet creation date')
 class TweetResource(Resource):
-    @api.marshal_with(tweet)
+    @api.marshal_with(json_tweet)
     def get(self, id):
         tweet = repository.get(id)
         if tweet is None:
-            api.abort(404)
+            api.abort(404, "Tweet {} doesn't exist".format(id))
         else:
             return tweet
-    @api.marshal_with(tweet)
-    def post(self, tweet):
-        if tweet is None:
-            api.abort(404)
+    @api.marshal_with(json_tweet, code=201)
+    @api.expect(json_new_tweet, validate=True)
+    def post(self):
+        text = api.payload["text"]
+        if len(text) > 0:
+            tweet = Tweet(text)
+            repository.add(tweet)
+            return tweet, 201
         else:
-            repository.post(tweet)
-            return repository.get(tweet.id)
+            return abort(422, "Tweet text can't be empty")
     @api.marshal_with(tweet)
-    def delete(self, tweet):
+    def delete(self, id):
+        tweet = repository.get(id)
         if tweet is None:
-            api.abort(404)
+            api.abort(404, "Tweet {} doesn't exist".format(id))
         else:
-            repository.delete(tweet)
-            return 200
-    @api.marshal_with(tweet)
-    def patch(self, tweet):
+            repository.remove(id)
+            return None
+    @api.marshal_with(json_tweet, code=200)
+    @api.expect(json_new_tweet, validate=True)
+    def patch(self, id):
+        tweet = repository.get(id)
         if tweet is None:
-            api.abort(404)
+            api.abort(404, "Tweet {} doesn't exist".format(id))
         else:
-            for tweet in repository.tweets:
-                if tweet == tweet:
-                    repository.tweets[tweet.id] = tweet
-            return 200
-
+            tweet.text = api.payload["text"]
+            return tweet
